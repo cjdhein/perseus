d8b55ecb987b1319b1e0f954eed7ba75c2978930
Yoav Gil <ygil@codyjdhein.com>
2018-10-12 05:09:07 +0000

    Set up Node.js server to respond with simple web page

---
+++ server/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+
+var app = express();
+
+app.set("port", 3000);
+
+app.get("/", function(req, res) {
+	res.type("text/plain");
+	res.send("Welcome to the main page!");
+});
+
+/*
+app.post("/form-data", function(req, res) {
+	res.type("application/xml");
+});
+*/
+
+app.use(function(req, res) {
+	res.type("text/plain");
+	res.status(404);
+	res.send("404 - Not Found");
+});
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.type("plain/text");
+	res.status(500);
+	res.send("500 - Server Error");
+});
+
+app.listen(app.get("port"), function() {
+	console.log("Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
+});
