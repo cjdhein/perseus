60e5201952179a1a7b497467e22297f54dd6fbbb
Cody Dhein <dheinc@oregonstate.edu>
2018-10-22 19:11:29 -0500

    Updated test.py

--- test.py
+++ test.py
@@ -3,12 +3,17 @@ from urlparse import urlparse
 import urllib2
 
 url = "https://www.gutenberg.org/files/11/11-h/11-h.htm"
-
+keyword = "chesire"
+print "Searching Alice in Wonderland for the word " + keyword
 html = urllib2.urlopen(url).read()
 
 soup = bs(html,'lxml')
 
 theText = soup.get_text().lower()
 
-uniSearch = unicode("gutenbe")
-print theText.count(uniSearch.lower())
+uniSearch = unicode(keyword)
+
+found = theText.count(uniSearch.lower())
+
+if found >= 1:
+    print "Found '" + keyword + "' in the text!"
