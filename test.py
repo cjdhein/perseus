5f83366e3260f3977aa90a265d2993d73ffb99a5
dheinc <cjdhein@codyjdhein.com>
2018-10-23 00:14:29 +0000

    test changes

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup as bs
 from urlparse import urlparse
 import urllib2
+import sys
 
 url = "https://www.gutenberg.org/files/11/11-h/11-h.htm"
-keyword = "chesire"
+keyword = str(sys.argv[1])
 print "Searching Alice in Wonderland for the word " + keyword
 html = urllib2.urlopen(url).read()
 
@@ -17,3 +18,5 @@ found = theText.count(uniSearch.lower())
 
 if found >= 1:
     print "Found '" + keyword + "' in the text!"
+else:
+    print keyword + " was not found in the text."
