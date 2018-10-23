ef68de138663a56b6d5da6f012382ccfc2bed8d9
Yoav Gil <ygil@codyjdhein.com>
2018-10-23 02:52:08 +0000

    Server handles POST data, prints to console. Server responds to POST request with xml file.

--- server/public/scripts/form_script.js
+++ server/public/scripts/form_script.js
@@ -1,27 +1,43 @@
+/* Override the event handler for the form "Submit" button.
+*  Send an asynchronous POST request to the server.
+*/
 document.getElementById("form").addEventListener("submit", function (event) {
-	//Commented out code to send a POST request to the server. This will be implemented next.
-	/*
+	sendPost();
+	event.preventDefault();
+});
+
+
+/* Send the form data to the server via HTTP POST.
+*  Asynchronously receive a response from the server.
+*/
+function sendPost() {
 	var req = new XMLHttpRequest();
 	payload = {
-		"createEntry": "createEntry",
 		url: document.getElementById("url").value,
 		breadth: document.getElementById("breadth").checked, //true for breadth, false for depth
 		limit: document.getElementById("limit").value,
 		keyword: document.getElementById("keyword").value,
 	};
-	req.open("POST", "/", true);
+	req.open("POST", "/post", true);
 	req.setRequestHeader("Content-Type", "application/json");
+
+	// Executes upon asynchronous response from server
 	req.addEventListener("load", function() {
 		var response = req.responseText;
-		
-		// Call function to display graphics
-
+		displayGraphics(response);
 	});
-	req.send(JSON.stringify(payload));
-	event.preventDefault();
-	*/
 
-	var textNode = document.createTextNode("Button clicked. ");
+	req.send(JSON.stringify(payload));
+}
+
+
+/* This function takes the XML response from the server and generates a graph.
+*  param: response (XML-formatted)
+*/
+function displayGraphics(response) {
+	// The following two lines are for testing, and can be commented out:
+	var textNode = document.createTextNode(response + " ");
 	document.body.appendChild(textNode);
-	event.preventDefault();
-});
+
+	// Add code here to display the XML response
+}
--- server/server.js
+++ server/server.js
@@ -1,25 +1,61 @@
-var express = require("express");
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
 var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+var path = "/public/log_files/"; //Path where log files are stored
 
 app.set("port", 3000);
 app.use(express.static("public"));
 
+// Homepage
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/public/html/main_page.html");
 });
 
-app.post("/", function(req, res) {
-	res.type("application/xml");
-	res.send("Post successfully received.")
+// POST request
+app.post("/post", function(req, res) {
+
+	// Deal with req data here, call data crawler
+	for (var param in req.body) {
+		console.log("Param: " + param + ", Value: " + req.body[param]);
+	}
+
+
+
+	var filename = "log_example_1.xml";
+
+	try {
+		if (fs.existsSync(__dirname + path + filename)) {
+			
+			console.log(filename + " exists."); //for testing
+
+			res.type("application/xml");
+			res.sendFile(__dirname + path + filename);
+
+		} else {
+			console.log(filename + " does not exist."); //for testing
+		}
+	} catch(err) {
+		console.error(err)
+	}
+
+	
+	//For testing:
+	//res.send("<?xml version=\"1.0\" ?><page>Post successfully received.</page>")
 });
 
+// 404
 app.use(function(req, res) {
 	res.type("text/plain");
 	res.status(404);
 	res.send("404 - Not Found");
 });
 
+// 500
 app.use(function(err, req, res, next) {
 	console.error(err.stack);
 	res.type("text/plain");
